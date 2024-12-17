860da3ac064f15cb6292d62b61a42949798f4e6f
hi-date-ns <date.hifumi.9fe@jp.nssol.nipponsteel.com>
2024-12-17 13:55:11 +0900

    タスク2（テーブルの本実装）

--- src/components/FormComponent.vue
+++ src/components/FormComponent.vue
@@ -16,14 +16,35 @@
     },
 
     setup(props) {
+      const YEAR = ['2022', '2023'];
+
       // [MEMO]テンプレートで使用するものを返す
-      return { props };
+      return { props, YEAR };
     },
   });
+
+  // export function createCalendar(): Calendar[] {
+
+  // }
 </script>
 
 <template>
   <form method="post" action="">
+    <div class="display:flex">
+      <select>
+        <option value="2024">2024</option>
+      </select>
+      <select>
+        <option value="12">12</option>
+      </select>
+      <select>
+        <option value="1">1</option>
+      </select>
+      <select>
+        <option value="食費">食費</option>
+        <option value="固定費">固定費</option>
+      </select>
+    </div>
     <div class="flex flex-col">
       <span>{{ props.textMessage }}</span>
       <input class="mt-1 px-1 py-1 textline border border-gray-900" type="text" id="text1" />
--- src/components/TableComponent.vue
+++ src/components/TableComponent.vue
@@ -1,6 +1,22 @@
 <script lang="ts">
   /* eslint-disable no-console*/
-  import { defineComponent } from 'vue';
+  import { defineComponent, ref } from 'vue';
+
+  type Calendar = {
+    // fullDate: Date;
+    year: number;
+    month: number;
+    date: number;
+    day: number;
+    // day: string;
+  };
+
+  type costTableDate = {
+    date: number;
+    day: string;
+    foodCost: number | null;
+    fixedCost: number | null;
+  };
 
   export default defineComponent({
     name: 'TableComponent',
@@ -36,21 +52,102 @@
       // ヘッダーラベル
       const HEDER_LABEL = {
         HEDER_1: '日付',
-        HEDER_2: '支出１',
-        HEDER_3: '支出２',
-        HEDER_4: '支出３',
-        HEDER_5: '支出４',
-        HEDER_6: '支出５',
-        HEDER_7: '支出６',
-        HEDER_8: '支出７',
-        HEDER_9: '支出８',
-        HEDER_10: '支出９',
+        HEDER_2: '曜日',
+        HEDER_3: '食費',
+        HEDER_4: '固定費',
       };
 
+      // 曜日ラベル
+      const DAYS = ['日', '月', '火', '水', '木', '金', '土'];
+
+      // カレンダー作成
+      const calendar: Calendar[] = createCalendar();
+      // 曜日色配列作成
+      const tableClass = ref(createDayColorArray(calendar));
+
+      // コストテーブルデータ配列作成
+      const costTableDateArray: costTableDate[] = createCostTableDate(calendar);
+
       // [MEMO]テンプレートで使用するものを返す
-      return { props, HEDER_LABEL, TABLE_DATES };
+      return { props, HEDER_LABEL, TABLE_DATES, costTableDateArray, tableClass };
     },
   });
+
+  /**
+   * カレンダー作成
+   */
+  export function createCalendar(): Calendar[] {
+    // 本日日付取得
+    const currentDate = new Date();
+    const nowYear = currentDate.getFullYear();
+    const nowMonth = currentDate.getMonth();
+
+    // 今月の一日取得
+    const firstDay = new Date(nowYear, nowMonth, 1);
+
+    //今月の最終日取得
+    const nextMonthFirstDay = new Date(nowYear, nowMonth + 1, 1);
+    nextMonthFirstDay.setDate(nextMonthFirstDay.getDate() - 1);
+    const lastDate = nextMonthFirstDay;
+
+    // 今月の全日付取得
+    const calendarDateArray: Date[] = [];
+    for (let i = 1; i <= lastDate.getDate(); i++) {
+      calendarDateArray.push(new Date(firstDay));
+      firstDay.setDate(firstDay.getDate() + 1);
+    }
+
+    // 戻り値作成
+    const calendarArray: Calendar[] = calendarDateArray.map(
+      (day): Calendar => ({
+        // fullDate: day,
+        year: day.getFullYear(),
+        month: day.getMonth() + 1,
+        date: day.getDate(),
+        day: day.getDay(),
+        // day: DAYS[day.getDay()],
+      })
+    );
+    return calendarArray;
+  }
+
+  /**
+   * 曜日色配列作成
+   * @param calendarArray
+   */
+  export function createDayColorArray(calendarArray: Calendar[]): string[] {
+    const dayColorArray: string[] = [];
+    calendarArray.map((calendarArray) => {
+      if (calendarArray.day === 0) {
+        dayColorArray.push('text-red-500');
+      } else if (calendarArray.day === 6) {
+        dayColorArray.push('text-blue-500');
+      } else {
+        dayColorArray.push('text-black-500');
+      }
+    });
+    return dayColorArray;
+  }
+
+  /**
+   * コストテーブルデータ配列作成
+   * @param calendarArray
+   */
+  export function createCostTableDate(calendarArray: Calendar[]) {
+    const DAYS = ['日', '月', '火', '水', '木', '金', '土'];
+
+    // 戻り値作成
+    const costTableDateArray: costTableDate[] = calendarArray.map(
+      (calendarArray): costTableDate => ({
+        date: calendarArray.date,
+        day: DAYS[calendarArray.day],
+        foodCost: null,
+        fixedCost: null,
+      })
+    );
+    console.log(costTableDateArray);
+    return costTableDateArray;
+  }
 </script>
 
 <template>
@@ -69,10 +166,15 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="(tableDate, index) in TABLE_DATES" :key="index">
-          <td v-for="(value, key, index) in tableDate" :key="'id' + index" class="cell table-auto border-collapse">
-            {{ value }}
+        <tr v-for="(costDate, index) in costTableDateArray" :key="index" :class="tableClass[index]">
+          <td class="cell">{{ costDate.date }}</td>
+          <!-- text-red-500 text-blue-500 text-gray-500 -->
+          <td class="cell">
+            {{ costDate.day }}
           </td>
+          <!-- <td v-for="(value, key, index) in calendarDate" :key="'id' + index" class="cell table-auto border-collapse">
+            {{ value }}
+          </td> -->
         </tr>
       </tbody>
     </table>
